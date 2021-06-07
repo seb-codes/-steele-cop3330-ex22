f24656f13591d0a0e83f2d04d5976533ad154088
Sebastian <SebastianLSteele@knights.ucf.edu>
2021-06-06 21:07:22 -0400

    we out here with commits

---
+++ src/main/java/org/example/CompareNumbers.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class CompareNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int largest_num=0;
+
+        System.out.print("Enter the first number: ");
+        Integer first = scan.nextInt();
+        System.out.print("Enter the second number: ");
+        Integer second = scan.nextInt();
+        System.out.print("Enter the third number: ");
+        Integer third = scan.nextInt();
+
+        if(first > second && first> third) {
+            largest_num = first;
+            System.out.println("The largest number is" + largest_num);
+        }
+        else if(second > third) {
+            largest_num = second;
+            System.out.println("The largest number is" + largest_num);
+        }
+        else if(third > second) {
+            largest_num = third;
+            System.out.println("The largest number is" + largest_num);
+        }
+        else System.out.println("Your values may be equal");
+
+    }
+}
